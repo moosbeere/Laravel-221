36500701373aa4804debdedbb9f584cd88eb21e8
moosbeere <moosbeere_O@mail.ru>
2023-09-19 15:05:36 +0300

    Lab 4

---
+++ resources/views/articles/main.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+@section('content')
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Name</th>
+      <th scope="col">ShortDesc</th>
+      <th scope="col">Desc</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach($articles as $article)
+    <tr>
+      <th scope="row">{{$article->date}}</th>
+      <td><a href="/article/show/{{$article->id}}"></a>{{$article->name}}</td>
+      <td>{{$article->shortDesc}}</td>
+      <td>{{$article->desc}}</td>
+    </tr>
+  @endforeach
+  </tbody>
+</table>
+@endsection
